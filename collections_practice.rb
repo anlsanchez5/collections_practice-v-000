59fb7d628400eb708794dec568ce557d4695367b
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-09-13 17:38:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  copy = array.sort_by {|x| x.downcase}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, from, to)
+  array[from], array[to] = array[to], array[from]
+  array
+end
+
+def reverse_array(array)
+  copy = array.reverse
+  copy
+end
+
+def kesha_maker(array)
+  array.collect { |string| string[2] = "$"}
+  array
+end
+
+def find_a(array)
+  string = array.join(" ")
+  string.scan(/\ba\w*/)
+end
+
+def sum_array(array)
+  array.inject(0) do |sum, element|
+    sum + element
+  end
+end
+
+def add_s(array)
+  array.collect_with_index do |ele, i|
+    next if i == 1
+    ele + "s"
+  end
+  array
+end
